ca55ff233b9763a273196b7c5557ef8777cee56a
Naol Daba <115622877+Naoldaba@users.noreply.github.com>
2024-05-23 11:21:32 +0300

    Update frog2.py

--- frog2.py
+++ frog2.py
@@ -1,12 +1,12 @@
 
-n, k =int(input())
+n, k = map(int, input().split())
 arr=list(map(int, input().split()))
 
-dp=[float('inf') for _ in range(n)]
+dp=[float('inf')]*n
 dp[0]=0
 
-for i in range(1, len(dp)):
-    for j in range(i-1, max(i-k, -1), -1):
+for i in range(n):
+    for j in range(i-1, max(i-1-k, -1), -1):
         dp[i]=min(dp[i], dp[j]+abs(arr[i]-arr[j]))
-        
+
 print(dp[n-1])
